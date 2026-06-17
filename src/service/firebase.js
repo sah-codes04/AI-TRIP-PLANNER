b51f60e75d4c44ef95bfafd4835be1311a13bffb
Satyam Amarjeet Sah <25278206+sah-codes04@users.noreply.github.com>
2026-06-17 12:47:08 +0530

    Remove debug logs

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -16,5 +16,4 @@ const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
-console.log("AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+
